151058e8d01096ca0a849bdb12abfbf489934829
clintgitahi <clinton@yumkenya.com>
2019-03-05 06:52:04 +0300

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by clinton gitahi on 05/03/2019.
+//  Copyright © 2019 clinton gitahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
